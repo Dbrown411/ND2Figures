5824d158a334bfb6562edff3b4979fda87f9819d
Dillon Brown <dillon411@gmail.com>
2021-08-17 16:22:05 -0400

    function organization

--- main.py
+++ main.py
@@ -6,31 +6,37 @@ import glob,os
 import numpy as np
 import cv2 as cv2
 
-def project_images(framestack,write=True,suffix=''):
+def framestack_to_vol(framestack):
     volume_shape = (len(framestack),framestack[0].shape[0],framestack[0].shape[1])
     vol = np.empty(volume_shape,dtype=np.uint16)
     for i,frame in enumerate(framestack):
         vol[i,:,:] = frame
+    return vol
+
+def project_images(framestack):
+    vol = framestack_to_vol(framestack)
     AIP = np.nanmean(vol[0:,:,:],axis=0,dtype=np.uint16)
-    if write:
-        cv2.imwrite(f"{outname}-AIP{suffix}{ext}", AIP)
+    return AIP
+
+def write_figure(fig,suffix=''):
+    cv2.imwrite(f"{outname}-AIP{suffix}{ext}", fig)
 
 directory = r"D:\OneDrive - Georgia Institute of Technology\Lab\Data\IHC\Confocal\Eyes\ACAN\01 - Control\Dual Stain Poly + Mono ACAN\Raw\MACAN and PNitige"
 for f in glob.glob(directory+os.sep+'*.nd2'):
     outname= f[:-4]
     ext = '.tif'
     with ND2Reader(f) as images:
-        try:
-            channels = images.metadata['channels']
-            channels_dict = {i:[] for i in range(len(channels))}
-            images.iter_axes = ['z','c']
-            for i,frame in enumerate(images):
-                channels_dict[i%len(channels)].append(frame)
-            for k,v in channels_dict.items():
-                project_images(v,suffix=f'-C{k}')
-        except Exception as e:
-            print(e)
+        channels = images.metadata['channels']
+        numChannels = len(channels)
 
-            project_images(images)
+        channels_dict = {i:[] for i in range(numChannels)}
+        images.iter_axes = ['z','c'][:-int(numChannels<2)]
+        for i,frame in enumerate(images):
+            channels_dict[i%numChannels].append(frame)
+        for k,v in channels_dict.items():
+            proj = project_images(v)
+            write_figure(proj,suffix=f'-C{k}')
+
+
 
 
